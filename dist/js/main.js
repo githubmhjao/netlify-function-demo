943ef4d7d7a8a1b9aacc6dbdc4cd25517339b1b6
githubmhjao <ntumhjao@gmail.com>
2021-05-29 16:30:10 +0800

    Update main.js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,15 @@
 const getPoeStatistics = async () => {
+  const leagues = ["ultimatum", "hardcore-ultimatum", "ssf-ultimatum", "ssf-ultimatum-hc"];
+  
+  const dataArray = await Promise.all(leagues.map(league => {
+    const url = `/poe/api/overview=${league}&type=exp&language=en`;
+    const res = awati fetch(url);
+    const { classNames, classes } = await res.json();
+    return { classNames, classes }
+  }))
+  
+  leagues.splice(0, 0, "classNames").forEach(league)
+  
   const res = await fetch("/poe/api/overview=ultimatum&type=exp");
   const { classNames, classes } = await res.json();
   
